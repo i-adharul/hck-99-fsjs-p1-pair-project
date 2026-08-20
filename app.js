90fc95bc7863dd3c0345f814b155d632f7d12f97
MAImron <i.adharul@gmail.com>
2026-08-20 16:39:47 +0700

    landing page and search

--- app.js
+++ app.js
@@ -3,23 +3,26 @@ const Controller = require('./controllers/controller');
 const app = express()
 const port = 3000
 
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 //setup router nanti jangan lupa
+//middleware & session
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
 })
 
-app.get('/', Controller.home)
-app.get('/register', Controller.home)
-app.get('/login', Controller.home)
-app.get('/cms', Controller.home) //jangan lupa protect role
-app.post('/cms', Controller.home)
-app.get('/coupons', Controller.home)
-app.get('/coupons/:couponId', Controller.home)
-app.get('/checkout', Controller.home)
-app.get('/payments', Controller.home)
-app.get('/payments/status', Controller.home)
-app.get('/payments/status/success', Controller.home)
-app.get('/profile', Controller.home)
+//butuh route logout
+app.get('/', Controller.landing)
+app.get('/register', Controller.register)
+app.get('/login', Controller.login)
+app.get('/cms', Controller.cms) //jangan lupa protect role
+app.post('/cms', Controller.postCoupon) //jangan lupa protect role
+app.get('/coupons', Controller.coupons)
+app.get('/coupons/:couponId', Controller.couponDetail)
+app.get('/checkout', Controller.checkout)
+app.get('/payments', Controller.payments)
+app.get('/payments/status', Controller.paymentStatus)
+app.get('/payments/status/success', Controller.paymentSuccess)
+app.get('/profile', Controller.profile)
--- controllers/controller.js
+++ controllers/controller.js
@@ -1,7 +1,131 @@
-class Controller{
-    static async home(req, res){
+const {
+    Category,
+    Coupon,
+    Order,
+    OrderItem,
+    Payment,
+    Profile,
+    User
+} = require('../models');
+const {Op} = require('sequelize')
+
+class Controller {
+    static async landing(req, res) {
         try {
-            
+            let { search } = req.query
+            let option = {
+                include: Category,
+                where: {}
+            }
+            if (search) {
+                option.where = {
+                    [Op.or]: [
+                        {
+                            title: {
+                                [Op.iLike]: `%${search}%`
+                            }
+                        },
+                        {
+                            description: {
+                                [Op.iLike]: `%${search}%`
+                            }
+                        }
+                    ]
+                }
+            }
+            let coupons = await Coupon.findAll(option)
+            res.render('landingPage', { coupons })
+        } catch (error) {
+            res.send(error.message)
+        }
+    }
+
+    static async register(req, res) {
+        try {
+
+        } catch (error) {
+            res.send(error)
+        }
+    }
+
+    static async login(req, res) {
+        try {
+
+        } catch (error) {
+            res.send(error)
+        }
+    }
+
+    static async cms(req, res) {
+        try {
+
+        } catch (error) {
+            res.send(error)
+        }
+    }
+
+    static async postCoupon(req, res) {
+        try {
+
+        } catch (error) {
+            res.send(error)
+        }
+    }
+
+    static async coupons(req, res) {
+        try {
+            let coupons = await Coupon.findAll({
+                include: Category
+            })
+            res.send(coupons)
+        } catch (error) {
+            res.send(error)
+        }
+    }
+
+    static async couponDetail(req, res) {
+        try {
+
+        } catch (error) {
+            res.send(error)
+        }
+    }
+
+    static async checkout(req, res) {
+        try {
+
+        } catch (error) {
+            res.send(error)
+        }
+    }
+
+    static async payments(req, res) {
+        try {
+
+        } catch (error) {
+            res.send(error)
+        }
+    }
+
+    static async paymentStatus(req, res) {
+        try {
+
+        } catch (error) {
+            res.send(error)
+        }
+    }
+
+    static async paymentSuccess(req, res) {
+        try {
+
+        } catch (error) {
+            res.send(error)
+        }
+    }
+
+    static async profile(req, res) {
+        try {
+
         } catch (error) {
             res.send(error)
         }
